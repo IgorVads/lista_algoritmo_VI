0eedb3cadd0bda5d7c744ebab234e64f4d5e4c8c
Igor Vinicius <161034058+IgorVads@users.noreply.github.com>
2024-04-17 17:25:03 -0300

    Update 3_laranjas.c
    Adicionado um comentário explicando o objetivo do código e alteração da indentação

--- 3_laranjas.c
+++ 3_laranjas.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 
+/*Um programa que solicita e lê a quantidade de maças desejadas, 
+calcula e informa o preço a ser pago pelo cliente, considerando os preços definidos 
+anteriormente: Um feirista vende suas laranjas a R$ 1,30 por unidade, se forem compradas menos de 
+uma dúzia, e a R$ 1,10 por unidade, se forem compradas pelo menos uma dúzia. 
+
+*/
+
 int main(){
 	
 	int quant;
 	float valorT;
 	
 	printf("Insira a quantidade desejada: ");
-	scanf("%i",&quant);
+		scanf("%i",&quant);
 	
 	if(quant < 12){
 		valorT = quant * 1.30;
