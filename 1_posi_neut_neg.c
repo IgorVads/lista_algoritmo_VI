e53ad104ae995ff0267b9406b64bca97bd1d829e
Igor Vinicius <161034058+IgorVads@users.noreply.github.com>
2024-04-17 17:37:53 -0300

    Update 1_posi_neut_neg.c
    Adicionado comentário informando o objetivo do programa, alterado printfs para uma melhor compreensão do usuário e alteração da indentação para uma melhor leitura do código para commits futuros.

--- 1_posi_neut_neg.c
+++ 1_posi_neut_neg.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
 #include <locale.h>
 
+/*Um programa que solicita e lê um valor inteiro e informa se ele é positivo, 
+negativo ou neutro.*/
+
 int main(){
     setlocale(LC_ALL, "portuguese-brazilian");
     int num;
 
-    printf("Insira um número: ");
-    scanf("%i",&num);
+    printf("Digite um número: ");
+        scanf("%d",&num);
 
-    if(num > 0 ){
-        printf("é positivo");
-    }else{
-        if(num < 0 ){
-            printf("é negativo");
-        }else{
-            printf("é neutro");
+    if(num > 0 )
+    {
+        printf("É positivo");
+    }else
+    {
+        if(num < 0 )
+        {
+            printf("É negativo");
         }
-
+        else{
+            printf("É neutro");
+        }
     }
-
-
-
 
     return(0);
 }
